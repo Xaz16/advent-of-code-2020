c37858d8a2619ed11b3ba4e7a29e684ffc4dc157
Xaz16 <gxaz16@gmail.com>
2020-12-27 00:02:52 +0300

    feat: :sparkles: Second day of Advent code 2020

---
+++ 2/main-first-part.mjs
@@ -0,0 +1,60 @@
+import { INPUT } from './input.mjs';
+
+
+/**
+ * @param  {string} password
+ * @param  {object} policy
+ */
+const isPasswordValid = (password, policy) => {
+    const passwordChars = password.split('')
+    const validForChars = [];
+    const invalidForChars = [];
+
+    for (const char in policy) {
+        const { min, max } = policy[char];
+        const findedChars = passwordChars.filter(item => item === char);
+        const isValidForChar = findedChars.length >= min && findedChars.length <= max;
+        const arrToPush = isValidForChar ? validForChars : invalidForChars;
+        arrToPush.push(char);
+    }
+
+    return {
+        valid: !invalidForChars.length,
+        invalidForChars,
+        validForChars
+    };
+}
+
+const getValidPasswords = (passPairs) => {
+    const validPasswords = [];
+    const invalidPasswords = [];
+
+    for (const pair of passPairs) {
+        const { input, policy } = pair;
+        const validationObj = isPasswordValid(input, policy);
+        const arrToPush = validationObj.valid ? validPasswords : invalidPasswords;
+        arrToPush.push(pair)
+    }
+
+    return {
+        validPasswords,
+        invalidPasswords
+    }
+}
+
+const parsedPass = INPUT.map(item => {
+    const [policy, password] = item.split(':');
+    const [range, char] = policy.split(' ');
+    const [min, max] = range.split('-');
+    const passwordObj = {
+        input: password,
+        policy: { [char]: { min, max } }
+    }
+
+    return passwordObj;
+})
+const { validPasswords } = getValidPasswords(parsedPass);
+console.log(`Answer is: ${validPasswords.length} valid passwords`);
+
+
+
