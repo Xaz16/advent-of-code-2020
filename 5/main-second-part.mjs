69156b94f616b6ec4887b96454cd16ffdf1ac4b4
Xaz16 <gxaz16@gmail.com>
2021-02-07 13:33:12 +0300

    feat: :sparkles: introduce 2th part of 5th day

---
+++ 5/main-second-part.mjs
@@ -0,0 +1,18 @@
+import { INPUT } from "./constants.mjs";
+
+const seatIds = INPUT.split("\n")
+  .map((item) => parseInt(item.replace(/(F|L)/gm, 0).replace(/(B|R)/gm, 1), 2))
+  .sort((a, b) => b - a);
+
+let answer;
+
+for (let i = 0; i < seatIds.length; i++) {
+  const currentNum = seatIds[i];
+  const nextNum = seatIds[i + 1];
+
+  if (nextNum && currentNum - nextNum !== 1) {
+    answer = currentNum - 1;
+  }
+}
+
+console.log(`Answer is: ${answer}`);
